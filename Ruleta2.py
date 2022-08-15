4671045cba79e9c38387c4853f862ee45e593b9e
elsamuel27 <110992469+elsamuel27@users.noreply.github.com>
2022-08-15 10:55:01 +0200

    Delete Ruleta2.py

--- Ruleta2.py
+++
@@ -1,9 +0,0 @@
-import random
-ruleta = []
-opt = input("Escribe todas las opciones que quieras\nPara elegir una de ellas aleatoriamente, escribe \"!\": ")
-while opt != "!":
-    ruleta.append(opt)
-    opt = input("Escribe otra opción: ")
-
-result = random.choice(ruleta)
-print(result)
